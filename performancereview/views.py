e5dc992d9d8707dc920beb35ccb518d240fb13ef
Chris Aguirre <17457620@student.westernsydney.edu.au>
2024-05-20 23:32:03 +1000

    Second commit

--- performancereview/views.py
+++ performancereview/views.py
@@ -1,4 +1,42 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Employee
+#Django already has login and logout functionality
+from django.contrib.auth import authenticate, login, logout
+#Displays error messages
+from django.contrib import messages
+
 
 def home(request):
-	return render(request, 'home.html', {})
+	employees = Employee.objects.all()
+	return render(request, 'home.html', {'employees':employees})
+
+def about(request):
+	return render(request, 'about.html', {})
+
+def login_user(request):
+	if request.method == "POST":
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		if user is not None:
+			login(request, user)
+			messages.success(request, ("Successfully logged in."))
+			return redirect('home')
+		else: 
+			messages.success(request, ("There was an error. Please try again."))
+			return redirect('login')
+			
+	else:
+		return render(request, 'login.html', {})
+
+def logout_user(request):
+	logout(request)
+	messages.success(request, ("Successfully logged out."))
+	return redirect('home')
+
+def performancereviewform(request):
+	return render(request, 'performancereviewform.html', {})
+
+
+
+
